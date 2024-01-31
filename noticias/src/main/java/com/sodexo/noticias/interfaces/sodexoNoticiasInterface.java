171dd38b3f6222e9ffc39957d9534041b12b8fd0
christopherpintoc <146459577+christopherpintoc@users.noreply.github.com>
2024-01-31 17:55:43 -0300

    Delete noticias directory

--- noticias/src/main/java/com/sodexo/noticias/interfaces/sodexoNoticiasInterface.java
+++
@@ -1,9 +0,0 @@
-package com.sodexo.noticias.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sodexo.noticias.modelo.Noticias;
-
-public interface sodexoNoticiasInterface extends JpaRepository<Noticias, Long>{
-
-}
